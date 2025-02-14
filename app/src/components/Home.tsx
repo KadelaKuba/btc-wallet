09c2436f5185c26d86f056ff11bd8f8b8ae71727
Jakub Kadela <kadelakuba@gmail.com>
2025-02-14 23:43:26 +0100

    get btc transactions by public key

--- app/src/components/Home.tsx
+++ app/src/components/Home.tsx
@@ -1,5 +1,26 @@
+import {useState} from "react";
+import {satoshisToBitcoin} from "bitcoin-conversion";
+
 export function Home() {
+    const [price, setPrice] = useState(0);
+
+    fetch('https://www.blockonomics.co/api/searchhistory', {
+        headers: {
+            Authorization: 'Bearer ' + import.meta.env.VITE_BLOCKONOMICS_API_KEY,
+            'Content-Type': 'application/json',
+        },
+        method: "POST",
+        body: JSON.stringify({"addr": import.meta.env.VITE_BTC_PUBLIC_KEY})
+    })
+        .then(resp => resp.json())
+        .then(function (response) {
+            const numberOfSatoshi = response.history.reduce((accumulator, currentValue) => accumulator + currentValue.value, 0)
+            const btcPrice = satoshisToBitcoin(numberOfSatoshi);
+
+            setPrice(btcPrice);
+        })
+
     return (
-        <>Test</>
+        <>{price}</>
     );
 }
