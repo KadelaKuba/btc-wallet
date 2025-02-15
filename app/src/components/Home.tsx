b3c7311afd6fa5fa1c988bec9ab27eede332ab39
Jakub Kadela <kadelakuba@gmail.com>
2025-02-15 21:07:28 +0100

    home - edit space on responsive devices

--- app/src/components/Home.tsx
+++ app/src/components/Home.tsx
@@ -15,13 +15,13 @@ export function Home() {
     return (
         <Paper>
             <Box p={4} justifyContent="center" alignItems="center">
-                <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Box display="flex" justifyContent="space-between" alignItems="center" mb={1}>
                     <Typography variant="h4">Bitcoin peněženka</Typography>
                     <Box>
                         <img src="/assets/bitcoinWallet.png" width={70} alt="BTC peněženka"/>
                     </Box>
                 </Box>
-                <Typography gutterBottom>Pro zobrazení stavu vašich BTC se prosím přihlaste!</Typography>
+                <Typography>Pro zobrazení stavu vašich BTC se prosím přihlaste!</Typography>
                 <Box pt={2}>
                     <Button variant="contained" onClick={handleLogin}>Přihlásit</Button>
                 </Box>
