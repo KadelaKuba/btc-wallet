766ff97df7b7a38457ac1a0e8985a727c973d3d9
Jakub Kadela <kadelakuba@gmail.com>
2025-02-17 17:09:38 +0100

    enhance transaction list

--- app/src/components/TransactionListItem.tsx
+++ app/src/components/TransactionListItem.tsx
@@ -1,5 +1,5 @@
-import {Box, Card, Divider, ListItem, Typography} from "@mui/material";
-import {CurrencyBitcoin} from "@mui/icons-material";
+import {Box, Card, ListItem, Typography} from "@mui/material";
+import {ArrowDownward} from "@mui/icons-material";
 import {satoshisToBitcoin} from "bitcoin-conversion";
 
 export function TransactionListItem({transaction}) {
@@ -8,20 +8,26 @@ export function TransactionListItem({transaction}) {
 
     return (
         <Box mb={1}>
-        <ListItem disableGutters disablePadding>
-            {/*<ListItemButton sx={{p: 0, width: '100%'}}>*/}
-                <Card variant="outlined" sx={{width: '100%'}}>
-                    <Box p={2} display="flex" alignItems="center" justifyContent="space-between">
-                        <Box display="flex" alignItems="center">
-                            <CurrencyBitcoin color="primary"/>
-                            {date.toLocaleString("cs")}
+            <ListItem disableGutters disablePadding>
+                {/*<ListItemButton sx={{p: 0, width: '100%'}}*/}
+                    <Card variant="outlined" sx={{width: '100%'}}>
+                        <Box p={2} display="flex" alignItems="center" justifyContent="space-between">
+                            <Box display="flex" alignItems="center">
+                                <ArrowDownward color="primary" fontSize="small"/>
+                                <Box pl={1}>
+                                    <Typography variant="body2">
+                                        Přijato
+                                    </Typography>
+                                    <Typography variant="body2">
+                                        {date.toLocaleString("cs")}
+                                    </Typography>
+                                </Box>
+                            </Box>
+                            <Typography>{totalAmountOfBtc.toFixed(5)} BTC</Typography>
                         </Box>
-                        <Typography>{totalAmountOfBtc.toFixed(5)} BTC</Typography>
-                    </Box>
-                </Card>
-            {/*</ListItemButton>*/}
-        </ListItem>
-    {/*<Divider/>*/}
+                    </Card>
+                {/*</ListItemButton>*/}
+            </ListItem>
         </Box>
     );
 }
